fbcbd79d6fe63f62b89a774e976360c5c1e4927c
Jorge Pérez <jperez@icf.unam.mx>
2017-09-14 16:34:37 -0500

    Add some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,13 @@
 using HORIZONS
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "Some basic tests" begin
+    apophisvt = vec_tbl("Apophis", CSV_FORMAT=true);
+    @test isa(apophisvt, String)
+    @test contains(apophisvt, "\$\$SOE")
+    @test contains(apophisvt, "\$\$EOE")
+    @test ismatch(r"\$\$SOE", apophisvt)
+    @test ismatch(r"\$\$EOE", apophisvt)
+    @test mSOE.offsets == Int64[]
+    @test mEOE.offsets == Int64[]
+end
