d762724edcc6d3247faa235764913f2bf5a239e7
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2019-09-06 01:09:24 -0500

    Attempt to get tests passing on travis

--- src/HORIZONS.jl
+++ src/HORIZONS.jl
@@ -10,7 +10,7 @@ using DelimitedFiles, Dates
 export horizons, vec_tbl, vec_tbl_csv, HORIZONS_MACHINE
 
 const HORIZONS_MACHINE = "ssd.jpl.nasa.gov"
-const HORIZONS_FTP_DIR = "pub/ssd"
+const HORIZONS_FTP_DIR = "/pub/ssd/"
 const HORIZONS_DATE_FORMAT = "yyyy-u-dd HH:MM:SS.sss"
 const ObjectName = Union{Int, String}
 const StartStopTime = Union{DateTime, Date, String}
