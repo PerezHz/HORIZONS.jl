43e933f7bf7095aba0f345b9292f9c63c42ccfc2
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2024-01-30 14:24:58 -0600

    Minor fix

--- src/HORIZONS.jl
+++ src/HORIZONS.jl
@@ -28,7 +28,7 @@ include("sbradar.jl")
 function __init__()
 
     # Breaking change warning added in v0.4.0; to be deleted in next minor version (v0.5.0)
-    @warn("""\n\n
+    @warn("""\n
         # Breaking change
         Starting from v0.4.0 HORIZONS.jl connects to JPL via a HTTP API.
         Previous versions used the telnet command line utility as an external dependency.
