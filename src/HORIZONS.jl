24591f1055660a8367b3043b81097710c0498158
Luis Eduardo Ramírez Montoya <luiseduardoramirezmontoya@ciencias.unam.mx>
2024-01-30 14:20:16 -0600

    Breaking change warning

--- src/HORIZONS.jl
+++ src/HORIZONS.jl
@@ -25,6 +25,16 @@ include("horizonsapi.jl")
 include("sbdb.jl")
 include("sbradar.jl")
 
-# TO DO: Add warning to indicate telnet interface changed to HTTP API
+function __init__()
+
+    # Breaking change warning added in v0.4.0; to be deleted in next minor version (v0.5.0)
+    @warn("""\n\n
+        # Breaking change
+        Starting from v0.4.0 HORIZONS.jl connects to JPL via a HTTP API.
+        Previous versions used the telnet command line utility as an external dependency.
+    """)
+
+end
+
 
 end # module
