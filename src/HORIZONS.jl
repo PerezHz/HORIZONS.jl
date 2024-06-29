658a27275c5c58b1dc823f562eb57d25a6567f46
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2024-06-29 21:23:07 +0200

    Move warning to deps/build.jl (fixes #41)

--- src/HORIZONS.jl
+++ src/HORIZONS.jl
@@ -29,16 +29,4 @@ include("obstbl.jl")
 include("scout.jl")
 include("ooetbl.jl")
 
-function __init__()
-
-    # Breaking change warning added in v0.4.0; to be deleted in next minor version (v0.5.0)
-    @warn("""\n
-        # Breaking change
-        Starting from v0.4.0 HORIZONS.jl connects to JPL via a HTTP API.
-        Previous versions used the telnet command line utility as an external dependency.
-    """)
-
-end
-
-
 end # module
